640ee72f08d48a85c5151ee8f74ca90207600cec
Jack Ma <jackma1206@gmail.com>
2020-02-10 20:50:27 -0500

    vue-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h3 class="form-heading">Columbia New Employee Intake Form</h3>
     <IntakeForm />
   </div>
 </template>
@@ -15,4 +16,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+#app {
+  width: 800px;
+  height: 100%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px 10px;
+  border: 1px solid black;
+}
+
+.form-heading {
+  text-align: center;
+}
+</style>
